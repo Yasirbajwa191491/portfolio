e6c858727a17e136fb68c4a6142517a0e850ceb5
Yasir Sohail <yasirsohail912@gmail.com>
2023-03-25 14:54:49 +0500

    change

--- src/components/About.js
+++ src/components/About.js
@@ -12,12 +12,7 @@ const About = () => {
           <p>Laravel / Vue Developer</p>
           <hr />
           <p style={{ color: "#696969" }}>
-            I have created different projects of React js with backend as well
-            with the help of Node js, Express js and MongoDB like WhatsApp
-            clone, Facebook clone, Weather websites with restful Api's, Todo
-            App, Notepad App and different portfolio websites. I have completed
-            my 6th months internship in IT service of Goverment College
-            University Faisalabad.
+           I am a full stack Developer
           </p>
 
           <img src="./wallpaper.jpg" alt="wallpaper" style={{height:"400px",width:"700px",margin:"auto"}} />
